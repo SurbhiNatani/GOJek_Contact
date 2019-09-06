44f9788dfdadee80aafa7fcf661003f48682d8e2
SurbhiNatani <38003401+SurbhiNatani@users.noreply.github.com>
2019-09-06 12:11:44 +0530

    Test

---
+++ GJIOS/GJIOSTests/GJIOSTests.swift
@@ -0,0 +1,38 @@
+//
+//  GJIOSTests.swift
+//  GJIOSTests
+//
+//  Created by Shurbhi Natani on 05/09/19.
+//  Copyright © 2019 Surbhi Natani. All rights reserved.
+//
+
+import XCTest
+//@testable import GJIOS
+
+class GJIOSTests: XCTestCase {
+
+    override func setUp() {
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+
+    func testExample() {
+        let int: Int? =  1
+        XCTAssertNil(int)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
